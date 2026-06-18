af37bc73a19700e65bcee357eeca534444bbacfe
keiichiro shikai <nb.jtktn.1211@gmail.com>
2026-06-19 01:32:29 +0900

    feat: お問い合わせ内容送信処理とサンクス画面の表示

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreContactRequest;
 use App\Models\Category;
+use App\Models\Contact;
 use App\Models\Tag;
 use Illuminate\View\View;
-use App\Http\Requests\StoreContactRequest;
 
 class ContactController extends Controller
 {
@@ -24,5 +25,25 @@ class ContactController extends Controller
         $category = Category::find($validated['category_id']);
 
         return view('contact.confirm', compact('validated', 'category'));
+    }
+
+    public function store(StoreContactRequest $request)
+    {
+        $validated = $request->validated();
+
+        $tagIds = $validated['tag_ids'] ?? [];
+
+        unset($validated['tag_ids']);
+
+        $contact = Contact::create($validated);
+
+        $contact->tags()->attach($tagIds);
+
+        return redirect('/thanks');
+    }
+
+    public function thanks()
+    {
+        return view('contact.thanks');
     }
 }
