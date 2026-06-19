ce5c52d0dbc0f586ad8582f3d8f1fb63249a91c4
keiichiro shikai <nb.jtktn.1211@gmail.com>
2026-06-20 01:20:09 +0900

    feat: 管理画面の検索&リセット機能の実装

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\IndexContactRequest;
 use App\Models\Category;
 use App\Models\Contact;
 use App\Models\Tag;
@@ -9,15 +10,50 @@ use Illuminate\View\View;
 
 class AdminController extends Controller
 {
-    public function index(): View
+    public function index(IndexContactRequest $request): View
     {
-        $contacts = Contact::with(['category', 'tags'])
+        $validated = $request->validated();
+
+        $query = Contact::with(['category', 'tags']);
+
+        $query->when(
+            ! empty($validated['keyword']),
+            function ($query) use ($validated) {
+                $query->where(function ($query) use ($validated) {
+                    $query->where('first_name', 'like', '%'.$validated['keyword'].'%')
+                        ->orWhere('last_name', 'like', '%'.$validated['keyword'].'%')
+                        ->orWhere('email', 'like', '%'.$validated['keyword'].'%');
+                });
+            }
+        );
+
+        $query->when(
+            ! empty($validated['gender']),
+            fn ($query) => $query->where('gender', $validated['gender'])
+        );
+
+        $query->when(
+            ! empty($validated['category_id']),
+            fn ($query) => $query->where('category_id', $validated['category_id'])
+        );
+
+        $query->when(
+            ! empty($validated['date']),
+            fn ($query) => $query->whereDate('created_at', $validated['date'])
+        );
+
+        $contacts = $query
             ->latest()
-            ->paginate(7);
+            ->paginate(7)
+            ->withQueryString();
 
         $categories = Category::all();
         $tags = Tag::all();
 
-        return view('admin.index', compact('contacts', 'categories', 'tags'));
+        return view('admin.index', compact(
+            'contacts',
+            'categories',
+            'tags'
+        ));
     }
 }
