95f650adaf08963f3b11849963c4f3a90d5237cf
keiichiro shikai <nb.jtktn.1211@gmail.com>
2026-06-30 15:40:41 +0900

    fix: telバリデーションにstring

--- app/Http/Requests/StoreContactRequest.php
+++ app/Http/Requests/StoreContactRequest.php
@@ -27,7 +27,7 @@ class StoreContactRequest extends FormRequest
             'last_name' => ['required', 'string', 'max:255'],
             'gender' => ['required', 'integer', 'in:1,2,3'],
             'email' => ['required', 'string', 'email', 'max:255'],
-            'tel' => ['required', 'regex:/^[0-9]{10,11}$/'],
+            'tel' => ['required', 'string', 'regex:/^[0-9]{10,11}$/'],
             'address' => ['required', 'string', 'max:255'],
             'building' => ['nullable', 'string', 'max:255'],
             'category_id' => ['required', 'integer', 'exists:categories,id'],
